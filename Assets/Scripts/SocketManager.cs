4fe078d0cd627db90fbfc7b5b7ef53202db31d70
Jaeyoung <jyko1101@gmail.com>
2024-12-17 16:01:44 +0900

    feat: ping pong test

--- Assets/Scripts/SocketManager.cs
+++ Assets/Scripts/SocketManager.cs
@@ -1,18 +1,47 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using SocketIO;
+using Quobject.SocketIoClientDotNet.Client;
+using SocketIOClient;
+using System.Threading.Tasks;
 
 public class SocketManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private SocketIOClient.SocketIO socket;
+    private const string serverURL = "http://localhost:3010";
+
     void Start()
     {
-        
+        // SocketIO 인스턴스를 생성하고 서버와 연결
+        socket = new SocketIOClient.SocketIO(serverURL);
+
+        // 연결이 성공하면 호출되는 이벤트 핸들러
+        socket.OnConnected += async (sender, e) =>
+        {
+            Debug.Log("[SocketIOClient] Connected to Server");
+            await PingTest();
+        };
+
+        // 'pong' 이벤트 리스닝
+        socket.On("pong", response =>
+        {
+            Debug.Log("[Pong] pong received");
+        });
+        // 연결 시작
+        socket.ConnectAsync();
     }
 
-    // Update is called once per frame
-    void Update()
+    // 핑 테스트를 위한 함수
+    async private Task PingTest()
     {
-        
+        // 서버에 'ping' 이벤트를 보냄
+        await socket.EmitAsync("ping", "ping data");
+        Debug.Log("[Ping] Ping test");
+    }
+
+    void OnApplicationQuit()
+    {
+        // 애플리케이션 종료 시 서버 연결 종료
+        socket.DisconnectAsync();
     }
 }
