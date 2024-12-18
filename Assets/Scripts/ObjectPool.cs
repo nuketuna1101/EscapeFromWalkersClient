991766d3720e4a448e32052a194e5eabfe417385
Jaeyoung <jyko1101@gmail.com>
2024-12-18 11:32:27 +0900

    fix: object pool generic to just gobj

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -1,15 +1,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool<T> where T : MonoBehaviour
+public class ObjectPool : MonoBehaviour
 {
-    private readonly Queue<T> pool = new Queue<T>();
+    private readonly Queue<GameObject> pool = new Queue<GameObject>();
     private readonly Transform parentTransform;
-    private readonly T prefab;
+    private readonly GameObject prefab;
     private readonly int initialCount = 20;
 
     // 초기화
-    public ObjectPool(T prefab, Transform parent = null)
+    public ObjectPool(GameObject prefab, Transform parent = null)
     {
         this.prefab = prefab;
         parentTransform = parent;
@@ -20,24 +20,24 @@ public class ObjectPool<T> where T : MonoBehaviour
     // 오브젝트 풀에 오브젝트 추가
     private void AddObjectToPool()
     {
-        T newObj = Object.Instantiate(prefab, parentTransform);
+        GameObject newObj = Object.Instantiate(prefab, parentTransform);
         newObj.gameObject.SetActive(false);
         pool.Enqueue(newObj);
     }
 
     // 오브젝트 할당
-    public T GetObject()
+    public GameObject GetObject()
     {
         if (pool.Count == 0)
             AddObjectToPool();
 
-        T obj = pool.Dequeue();
+        GameObject obj = pool.Dequeue();
         obj.gameObject.SetActive(true);
         return obj;
     }
 
     // 오브젝트 회수
-    public void ReturnObject(T obj)
+    public void ReturnObject(GameObject obj)
     {
         obj.gameObject.SetActive(false);
         pool.Enqueue(obj);
