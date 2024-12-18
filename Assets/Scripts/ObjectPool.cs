1d75b426b606cfb764f78fc9eb646cbc816617d5
Jaeyoung <jyko1101@gmail.com>
2024-12-18 11:29:25 +0900

    feat: generic object pool sys

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -1,18 +1,45 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool : MonoBehaviour
+public class ObjectPool<T> where T : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private readonly Queue<T> pool = new Queue<T>();
+    private readonly Transform parentTransform;
+    private readonly T prefab;
+    private readonly int initialCount = 20;
+
+    // 초기화
+    public ObjectPool(T prefab, Transform parent = null)
     {
-        
+        this.prefab = prefab;
+        parentTransform = parent;
+        for (int i = 0; i < initialCount; i++)
+            AddObjectToPool();
     }
 
-    // Update is called once per frame
-    void Update()
+    // 오브젝트 풀에 오브젝트 추가
+    private void AddObjectToPool()
     {
-        
+        T newObj = Object.Instantiate(prefab, parentTransform);
+        newObj.gameObject.SetActive(false);
+        pool.Enqueue(newObj);
+    }
+
+    // 오브젝트 할당
+    public T GetObject()
+    {
+        if (pool.Count == 0)
+            AddObjectToPool();
+
+        T obj = pool.Dequeue();
+        obj.gameObject.SetActive(true);
+        return obj;
+    }
+
+    // 오브젝트 회수
+    public void ReturnObject(T obj)
+    {
+        obj.gameObject.SetActive(false);
+        pool.Enqueue(obj);
     }
 }
