3cbcbde66d5aa49dd134c4e0e9e866b335b937f9
Jaeyoung <jyko1101@gmail.com>
2024-12-18 11:25:04 +0900

    feat: location update

--- Assets/Scripts/SocketManager.cs
+++ Assets/Scripts/SocketManager.cs
@@ -1,14 +1,44 @@
-using System.Collections;
-using UnityEngine;
-using SocketIO;
-using Quobject.SocketIoClientDotNet.Client;
-using SocketIOClient;
+//using System.Collections;
+//using UnityEngine;
+//using SocketIO;
+//using Quobject.SocketIoClientDotNet.Client;
+//using SocketIOClient;
+//using System.Threading.Tasks;
 using System.Threading.Tasks;
+using UnityEngine;
+using SocketIOClient;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 public class SocketManager : MonoBehaviour
 {
+    public static SocketManager Instance { get; private set; }
+
     private SocketIOClient.SocketIO socket;
     private const string serverURL = "http://localhost:3010";
+
+
+    public GameObject playerPrefab; // 플레이어 프리팹
+    private Dictionary<string, GameObject> players = new Dictionary<string, GameObject>();
+
+
+    // 싱글턴 인스턴스 설정
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // 씬이 바뀌어도 유지
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
 
     void Start()
     {
@@ -27,6 +57,34 @@ public class SocketManager : MonoBehaviour
         {
             Debug.Log("[Pong] pong received");
         });
+
+        // 위치 업데이트
+        socket.On("locationUpdate", response =>
+        {
+            Debug.Log("[Test] test");
+            // response에서 uuid와 position을 꺼내서 로그로 출력
+            try
+            {
+                var playerData = response.GetValue<ResponseDTO>();
+                Debug.Log($"[LocationUpdate] UUID: {playerData.uuid}, Position: {playerData.position.ToVector3()}");
+                // 플레이어가 존재하지 않으면 새로 생성
+                if (!players.ContainsKey(playerData.uuid))
+                {
+                    GameObject newPlayer = Instantiate(playerPrefab, playerData.position.ToVector3(), Quaternion.identity);
+                    players.Add(playerData.uuid, newPlayer);
+                }
+                else
+                {
+                    // 존재하면 위치 업데이트
+                    players[playerData.uuid].transform.position = playerData.position.ToVector3();
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("[Error] Exception occurred: " + e.Message);
+            }
+        });
+
         // 연결 시작
         socket.ConnectAsync();
     }
@@ -44,4 +102,16 @@ public class SocketManager : MonoBehaviour
         // 애플리케이션 종료 시 서버 연결 종료
         socket.DisconnectAsync();
     }
+
+    public void SendPlayerPosition(Vector3 position)
+    {
+        var positionData = new
+        {
+            uuid = socket.Id,
+            position = new { x = position.x, y = position.y, z = position.z }
+        };
+        socket.EmitAsync("location", positionData);
+    }
 }
+
+
